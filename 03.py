1c72592d801007f0fc249634f4dfd112683de0fe
hsinhuibiga <hsinhuibiga@gmail.com>
2020-04-17 13:03:41 +1000

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,23 @@
+#Hamming Distance
+
+###
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+###
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ham = x ^ y  # hamming distance表一個1一個0的數量，取x XOR y後，再算bit 1的個數
+        cnt = 0
+
+        while ham:  # 當ham不等於0
+            ham &= (ham - 1)  # 和自己減1互相XOR會將最後一位的bit 1去除掉，所以反覆做到ham為0時的總次數即為答案
+            cnt += 1
+
+        return cnt
